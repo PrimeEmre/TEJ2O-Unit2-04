f87bd79278b720b42a2022576a8a9e690ea9b62a
Patrick Coxall <mr.coxall@mths.ca>
2023-08-25 13:56:16 +0000

    start of python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This module simulates a 6 sided dice
 """
 
 from microbit import *
+from time import sleep
 
 
-display.scroll("Hello, World!")
+# our variable for a random number
+randomNumber = 0
+
+display.clearScreen()
+sleep(1000)
+
+
